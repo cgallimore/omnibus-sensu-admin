f549ca9e66f0f67dacc4075bc30db8c6098a6ddf
cgallimore <chris.gallimore@sonian.net>
2013-08-16 07:31:40 -0700

    add god monitor

--- config/software/pre_install.rb
+++ config/software/pre_install.rb
@@ -2,7 +2,7 @@ name "pre_install"
 #version "1.4.5"
 
 #dependencies ["ruby", "rubygems"]
-gems = {"bcrypt-ruby" => "3.1.1", "eventmachine" => "1.0.3", "faye-websocket" => "0.4.7", "http_parser.rb" => "0.5.3", "json" => "1.8.0", "kgio" => "2.8.0", "rack" => "1.4.5", "raindrops" => "0.11.0", "rake" => "0.9.2.2", "sqlite3" => "1.3.7", "therubyracer" => "0.11.4", "thin" => "1.5.1", "unicorn" => "4.6.3"}
+gems = {"bcrypt-ruby" => "3.1.1", "eventmachine" => "1.0.3", "faye-websocket" => "0.4.7", "god" => "0.13.2", "http_parser.rb" => "0.5.3", "json" => "1.8.0", "kgio" => "2.8.0", "rack" => "1.4.5", "raindrops" => "0.11.0", "rake" => "0.9.2.2", "sqlite3" => "1.3.7", "therubyracer" => "0.11.4", "thin" => "1.5.1", "unicorn" => "4.6.3"}
 
 build do
   gems.each do |name, version|
